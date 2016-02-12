3ffaf74e18662d82dd4072333d3d4620e784c2e4
nashmaniac <nishantve1@gmail.com>
2016-02-12 16:23:37 +0530

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,3 @@
+// You can simply begin writing javascript and webpack has all the backdoors covered for you no need to worry 
+// about anything 
+console.log('This is something that I am very proud of ');
